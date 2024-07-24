2f95ec6345cb8777675fc09772c9be017ccaad4c
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-07-24 10:09:41 +0300

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_created=True)
+    deadline = models.DateTimeField()
+    checker = models.BooleanField()
+    tags = models.ForeignKey("Tag", on_delete=models.SET_NULL)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
